0100e05efb57117d9c3ef2f10cbdc70bd58e5718
DevChris <developer@asjurfinlegaltech.com>
2024-09-22 12:34:14 -0500

    feacture-trip: clase trip serializable para el manejo de listas

--- app/src/main/java/logica/Trip.kt
+++ app/src/main/java/logica/Trip.kt
@@ -1,38 +1,34 @@
 import android.os.Build
 import androidx.annotation.RequiresApi
+import java.io.Serializable
 import java.time.LocalDate
-
 import java.time.temporal.ChronoUnit
 
 class Trip(
-    val id: Int,
+    val id: Int = (1..100).random(), // Genera un ID aleatorio si no se pasa uno
     val startDate: LocalDate,
     val endDate: LocalDate,
     val destination: String
-) {
+) : Serializable {
 
     companion object {
         fun createTrip(id: Int, startDate: LocalDate, endDate: LocalDate, destination: String): Trip {
             return Trip(id, startDate, endDate, destination)
         }
     }
-    // Calcula la duración del viaje en días
+
     @RequiresApi(Build.VERSION_CODES.O)
     fun tripDuration(): Long {
         return ChronoUnit.DAYS.between(startDate, endDate)
     }
 
-    // Verifica si una fecha está dentro del rango del viaje
     @RequiresApi(Build.VERSION_CODES.O)
     fun isDateInRange(date: LocalDate): Boolean {
         return !date.isBefore(startDate) && !date.isAfter(endDate)
     }
 
-    // Devuelve una descripción del viaje
     @RequiresApi(Build.VERSION_CODES.O)
     fun tripDetails(): String {
         return "Viaje a $destination del $startDate al $endDate (Duración: ${tripDuration()} días)"
     }
 }
-
-
